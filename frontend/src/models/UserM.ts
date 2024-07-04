03eb5f01f79d52135b38bb470d6c70b18732200f
FerUgalde <fernanda.ugalde@uabc.edu.mx>
2024-07-03 21:44:13 -0700

    Register in Frontend and Backend - added photo field

---
+++ frontend/src/models/UserM.ts
@@ -0,0 +1,20 @@
+interface UserM{
+    id?: number;
+    email: string;
+    password?: string;
+    first_name: string;
+    last_name: string;
+    role: number;
+    study_area?: string;
+    booking?: Date;
+    specialties?: string;
+    hourly_rate?: number;
+    experience?: string;
+    availability?: number[];
+    photo?: string;
+    messages?: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export default UserM;
