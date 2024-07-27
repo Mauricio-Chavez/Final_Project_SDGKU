d847a21ca1e31fd07baaf45e701bfaaeac7dad5b
Mauricio-Chavez <mau_chavez003@hotmail.com>
2024-07-27 16:11:56 -0700

    fixing bugs in version 2 of styles

--- frontend/src/pages/auth/Register/Register.tsx
+++ frontend/src/pages/auth/Register/Register.tsx
@@ -3,10 +3,9 @@ import authService from '../../../service/auth.service';
 import { useForm } from 'react-hook-form';
 import UserM from '../../../models/UserM';
 import { useNavigate } from 'react-router-dom';
-import { Input, Radio, Select, Option } from '@material-tailwind/react';
-import { useState } from 'react';
+import { Input, Radio, Select, Option, Typography } from '@material-tailwind/react';
+import { useState, useRef } from 'react';
 import { generalData } from '../../../common/generalData';
-
 
 const Register = () => {
   const {
@@ -19,6 +18,7 @@ const Register = () => {
   const navigate = useNavigate();
 
   const [photo, setPhoto] = useState<File | null>(null);
+  const photoInputRef = useRef<HTMLInputElement | null>(null);
 
   const handlePhoto = (e: any) => {
     const selectedPhoto = e.target.files[0];
@@ -57,170 +57,130 @@ const Register = () => {
 
   return (
     <div className='user-register'>
-      <h1>Register</h1>
-
-      <form onSubmit={handleSubmit(handleRegister)}>
-        <div>
-          <Input
-            label='Email'
-            type='email'
-            placeholder='Email'
-            required
-            {...register('email', { required: 'Email is required' })}
-            crossOrigin=''
-            onPointerEnterCapture={() => { }}
-            onPointerLeaveCapture={() => { }}
-          />
-          {/* <input
-            type='text'
-            {...register('email', { required: 'Email is required' })}
-            placeholder='Email'
-          />
-           */}
-        </div>
-        <div>
-          <Input
-            label='Password'
-            type='password'
-            placeholder='Password'
-            required
-            {...register('password', { required: 'Password is required' })}
-            crossOrigin=''
-            onPointerEnterCapture={() => { }}
-            onPointerLeaveCapture={() => { }}
-          />
-          {errors.password && <span>{errors.password.message}</span>}
-          {/* <input
-            type='password'
-            {...register('password', { required: 'Password is required' })}
-            placeholder='Password'
-          /> */}
-        </div>
-        <div>
-          <Input
-            label='First Name'
-            type='text'
-            placeholder='First name'
-            required
-            {...register('first_name', { required: 'First name is required' })}
-            crossOrigin=''
-            onPointerEnterCapture={() => { }}
-            onPointerLeaveCapture={() => { }}
-          />
-          {errors.first_name && <span>{errors.first_name.message}</span>}
-          {/* <input
-            type='text'
-            {...register('first_name', { required: 'First name is required' })}
-            placeholder='First name'
-          /> */}
-        </div>
-        <div>
-          <Input
-            label='Last Name'
-            type='text'
-            placeholder='Last name'
-            required
-            {...register('last_name', { required: 'Last name is required' })}
-            crossOrigin=''
-            onPointerEnterCapture={() => { }}
-            onPointerLeaveCapture={() => { }}
-          />
-          {errors.last_name && <span>{errors.last_name.message}</span>}
-          {/* <input
-            type='text'
-            {...register('last_name', { required: 'Last name is required' })}
-            placeholder='Last name'
-          /> */}
-        </div>
-        <div>
-          <Select
-            label='Select Study Area'
-            placeholder='Select Study Area'
-            onChange={(value) => setValue('study_area', parseInt(value || ''))}
-            onPointerEnterCapture={() => { }}
-            onPointerLeaveCapture={() => { }}
+      <div className='img-container'>
+        <img src={`${process.env.PUBLIC_URL}/media/logo.png`} alt="" />
+      </div>
+      <div className='form-container'>
+        <h1 className="title">Login</h1>
+        <form onSubmit={handleSubmit(handleRegister)}>
+          <div
+            className='photo-container'
+            onClick={() => photoInputRef.current?.click()}
           >
-            {generalData.study_area.map((option) => (
-              <Option key={option.value} value={option.value.toString()}>
-                {option.label}
-              </Option>
-            ))}
-          </Select>
-          {/* <input
-            type='text'
-            {...register('study_area')}
-            placeholder='Study area'
-          /> */}
-        </div>
-        <div>
-          <Radio
-            id='tutee'
-            label='Tutee'
-            {...register('role', { required: 'Role is required' })}
-            value='0'
-            checked={selectedRole === 0}
-            onChange={() => setValue('role', 0)}
-            required
-            crossOrigin=''
-            onPointerEnterCapture={() => { }}
-            onPointerLeaveCapture={() => { }}
-          />
-          <Radio
-            id='tutor'
-            label='Tutor'
-            {...register('role', { required: 'Role is required' })}
-            value='1'
-            checked={selectedRole === 1}
-            onChange={() => setValue('role', 1)}
-            required
-            crossOrigin=''
-            onPointerEnterCapture={() => { }}
-            onPointerLeaveCapture={() => { }}
-          />
-          {errors.role && <span>{errors.role.message}</span>}
-        </div>
-        {/* <div>
-          <label>
-            <input
-              type="radio"
-              {...register('role', { required: 'Role is required' })}
-              value='0'
-              checked={selectedRole === 0}
-              onChange={() => setValue('role', 0)}
+            <img src={photo ? URL.createObjectURL(photo) : 'https://via.placeholder.com/150'} alt="user-photo" className='profile-img' />
+          </div>
+
+          <div className='inputs-container'>
+            <Input
+              label='Email'
+              type='email'
+              placeholder='Email'
+              required
+              {...register('email', { required: 'Email is required' })}
+              crossOrigin=''
+              onPointerEnterCapture={() => { }}
+              onPointerLeaveCapture={() => { }}
             />
-            Tutee
-          </label>
-          <label>
-            <input
-              type="radio"
-              {...register('role', { required: 'Role is required' })}
-              value='1'
-              checked={selectedRole === 1}
-              onChange={() => setValue('role', 1)}
+
+
+            <Input
+              label='Password'
+              type='password'
+              placeholder='Password'
+              required
+              {...register('password', { required: 'Password is required' })}
+              crossOrigin=''
+              onPointerEnterCapture={() => { }}
+              onPointerLeaveCapture={() => { }}
             />
-            Tutor
-          </label>
-          {errors.role && <span>{errors.role.message}</span>}
-        </div> */}
-        <div>
-          <div>
+            {errors.password && <span>{errors.password.message}</span>}
+
+
+            <Input
+              label='First Name'
+              type='text'
+              placeholder='First name'
+              required
+              {...register('first_name', { required: 'First name is required' })}
+              crossOrigin=''
+              onPointerEnterCapture={() => { }}
+              onPointerLeaveCapture={() => { }}
+            />
+            {errors.first_name && <span>{errors.first_name.message}</span>}
+
+
+            <Input
+              label='Last Name'
+              type='text'
+              placeholder='Last name'
+              required
+              {...register('last_name', { required: 'Last name is required' })}
+              crossOrigin=''
+              onPointerEnterCapture={() => { }}
+              onPointerLeaveCapture={() => { }}
+            />
+            {errors.last_name && <span>{errors.last_name.message}</span>}
+
+
+            <Select
+              label='Select Study Area'
+              placeholder='Select Study Area'
+              onChange={(value) => setValue('study_area', parseInt(value || ''))}
+              onPointerEnterCapture={() => { }}
+              onPointerLeaveCapture={() => { }}
+            >
+              {generalData.study_area.map((option) => (
+                <Option key={option.value} value={option.value.toString()}>
+                  {option.label}
+                </Option>
+              ))}
+            </Select>
+
+
+            <div className='radio-container'>
+              <Radio
+                id='tutee'
+                label='Tutee'
+                {...register('role', { required: 'Role is required' })}
+                value='0'
+                checked={selectedRole === 0}
+                onChange={() => setValue('role', 0)}
+                required
+                crossOrigin=''
+                onPointerEnterCapture={() => { }}
+                onPointerLeaveCapture={() => { }}
+              />
+              <Radio
+                id='tutor'
+                label='Tutor'
+                {...register('role', { required: 'Role is required' })}
+                value='1'
+                checked={selectedRole === 1}
+                onChange={() => setValue('role', 1)}
+                required
+                crossOrigin=''
+                onPointerEnterCapture={() => { }}
+                onPointerLeaveCapture={() => { }}
+              />
+            </div>
+            {errors.role && <span>{errors.role.message}</span>}
             <input
               type="file"
               name='photo'
               accept='image/*'
               onChange={handlePhoto}
+              ref={photoInputRef}
+              className='input-photo'
             />
           </div>
-          <div className="grid min-h-[140px] w-[280px] place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
-            <p>Photo Preview</p>
-            <img src={photo ? URL.createObjectURL(photo) : 'https://via.placeholder.com/150'} alt="user-photo" className='object-cover object-center rounded-full h-46 w-46' />
-          </div>
-        </div>
-        <div className='btn'>
-          <button type='submit'>Register</button>
-        </div>
-      </form>
 
+
+
+        </form>
+        <div className='btn'>
+            <button type='submit'>Register</button>
+        </div>
+      </div>
     </div>
   )
 }
